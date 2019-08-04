6c0fbcbde2da90902d284a0ec12754003c771ff6
Vanita Bhoyar <vanita.bhoyar282@gmail.com>
2019-08-04 01:04:54 -0700

    Added Event binding logic

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,17 +9,21 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = "offline";
   allowNewServer = false;
-
+  serverCreationStatus = "No server was created";
   constructor() {
     setTimeout(() => {
-      this.allowNewServer=true;
-    },2000)
-   }
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  onCreatedServer(){
+    this.serverCreationStatus="server was created";
+  }
 
   getServerStatus() {
     return this.serverStatus;
   }
 
-  
+
   ngOnInit() { }
 }
